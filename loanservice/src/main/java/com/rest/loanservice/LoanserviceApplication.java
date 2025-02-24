8275ed844b15167e1fb7e98b73ece1a35e3fca61
Ivan Y. Avila <ivan.y.avila@outlook.com>
2025-02-24 17:24:43 -0600

    ADD: Eureka client service to each rest API

--- loanservice/src/main/java/com/rest/loanservice/LoanserviceApplication.java
+++ loanservice/src/main/java/com/rest/loanservice/LoanserviceApplication.java
@@ -2,8 +2,10 @@ package com.rest.loanservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LoanserviceApplication {
 
 	public static void main(String[] args) {
